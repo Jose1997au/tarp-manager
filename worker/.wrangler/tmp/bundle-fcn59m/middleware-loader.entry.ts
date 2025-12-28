6134963d16a29c1a4e9d99d669dfb61aceebaf7e
Jose1997au <jose1997au@gmail.com>
2025-12-28 11:41:39 -0800

    Update wrangler build output and config formatting
    Renamed and updated temporary bundle and dev files in .wrangler/tmp to use new build identifiers. Updated import paths in middleware-loader.entry.ts to match new bundle directory. Minor formatting change in wrangler.jsonc (removed trailing comma).

--- worker/.wrangler/tmp/bundle-fcn59m/middleware-loader.entry.ts
+++ worker/.wrangler/tmp/bundle-fcn59m/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\josea\\OneDrive\\Desktop\\tarp-manager\\worker\\.wrangler\\tmp\\bundle-8y20wM\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\josea\\OneDrive\\Desktop\\tarp-manager\\worker\\.wrangler\\tmp\\bundle-fcn59m\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\josea\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\Users\\josea\\OneDrive\\Desktop\\tarp-manager\\worker\\.wrangler\\tmp\\bundle-8y20wM\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\Users\\josea\\OneDrive\\Desktop\\tarp-manager\\worker\\.wrangler\\tmp\\bundle-fcn59m\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\Users\\josea\\OneDrive\\Desktop\\tarp-manager\\worker\\.wrangler\\tmp\\bundle-8y20wM\\middleware-insertion-facade.js";
+export * from "C:\\Users\\josea\\OneDrive\\Desktop\\tarp-manager\\worker\\.wrangler\\tmp\\bundle-fcn59m\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
